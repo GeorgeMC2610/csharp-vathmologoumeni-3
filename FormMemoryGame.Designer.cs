ff6771fb06d0374633ab6d5b0e1160cd025f6d8b
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-02-21 16:57:05 +0200

    main menu functions implemented

--- FormMemoryGame.Designer.cs
+++ FormMemoryGame.Designer.cs
@@ -29,40 +29,41 @@ namespace csharp_vathmologoumeni_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonReturnToMenu = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
             this.Player_Name = new System.Windows.Forms.Label();
             this.Memory_Game_picture = new System.Windows.Forms.PictureBox();
-            this.LeaderBoard = new System.Windows.Forms.Label();
-            this.Lets_Play = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.linkedLabelLeaderboard = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.Memory_Game_picture)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonReturnToMenu
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(902, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 47);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Return to main menu";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonReturnToMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReturnToMenu.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.buttonReturnToMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.buttonReturnToMenu.ForeColor = System.Drawing.Color.Red;
+            this.buttonReturnToMenu.Location = new System.Drawing.Point(902, 11);
+            this.buttonReturnToMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonReturnToMenu.Name = "buttonReturnToMenu";
+            this.buttonReturnToMenu.Size = new System.Drawing.Size(101, 47);
+            this.buttonReturnToMenu.TabIndex = 6;
+            this.buttonReturnToMenu.Text = "Return to main menu";
+            this.buttonReturnToMenu.UseVisualStyleBackColor = false;
+            this.buttonReturnToMenu.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // textBoxUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 38);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(135, 23);
-            this.textBox1.TabIndex = 1;
+            this.textBoxUsername.Location = new System.Drawing.Point(12, 38);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(135, 23);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBoxUsername_TextChanged);
             // 
             // Player_Name
             // 
@@ -71,7 +72,7 @@ namespace csharp_vathmologoumeni_3
             this.Player_Name.Location = new System.Drawing.Point(12, 18);
             this.Player_Name.Name = "Player_Name";
             this.Player_Name.Size = new System.Drawing.Size(60, 15);
-            this.Player_Name.TabIndex = 2;
+            this.Player_Name.TabIndex = 99;
             this.Player_Name.Text = "Username";
             // 
             // Memory_Game_picture
@@ -89,70 +90,89 @@ namespace csharp_vathmologoumeni_3
             this.Memory_Game_picture.TabIndex = 3;
             this.Memory_Game_picture.TabStop = false;
             // 
-            // LeaderBoard
+            // buttonPlay
             // 
-            this.LeaderBoard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.LeaderBoard.AutoSize = true;
-            this.LeaderBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.LeaderBoard.Location = new System.Drawing.Point(696, 36);
-            this.LeaderBoard.Name = "LeaderBoard";
-            this.LeaderBoard.Size = new System.Drawing.Size(91, 17);
-            this.LeaderBoard.TabIndex = 4;
-            this.LeaderBoard.Text = "LeaderBoard";
+            this.buttonPlay.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonPlay.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonPlay.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlay.ForeColor = System.Drawing.Color.Red;
+            this.buttonPlay.Location = new System.Drawing.Point(442, 11);
+            this.buttonPlay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(130, 47);
+            this.buttonPlay.TabIndex = 4;
+            this.buttonPlay.Text = "Lets Play!";
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
             // 
-            // Lets_Play
+            // numericUpDownMinutes
             // 
-            this.Lets_Play.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Lets_Play.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Lets_Play.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lets_Play.ForeColor = System.Drawing.Color.Red;
-            this.Lets_Play.Location = new System.Drawing.Point(442, 11);
-            this.Lets_Play.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Lets_Play.Name = "Lets_Play";
-            this.Lets_Play.Size = new System.Drawing.Size(130, 47);
-            this.Lets_Play.TabIndex = 7;
-            this.Lets_Play.Text = "Lets Play!";
-            this.Lets_Play.UseVisualStyleBackColor = true;
-            this.Lets_Play.Click += new System.EventHandler(this.Lets_Play_Click);
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(153, 38);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
-            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDownMinutes.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(186, 38);
+            this.numericUpDownMinutes.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownMinutes.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownMinutes.TabIndex = 3;
+            this.numericUpDownMinutes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(153, 17);
+            this.checkBox1.Location = new System.Drawing.Point(186, 18);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(122, 19);
-            this.checkBox1.TabIndex = 9;
+            this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "Countdown Timer";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // linkedLabelLeaderboard
+            // 
+            this.linkedLabelLeaderboard.AutoSize = true;
+            this.linkedLabelLeaderboard.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkedLabelLeaderboard.Location = new System.Drawing.Point(687, 32);
+            this.linkedLabelLeaderboard.Name = "linkedLabelLeaderboard";
+            this.linkedLabelLeaderboard.Size = new System.Drawing.Size(127, 25);
+            this.linkedLabelLeaderboard.TabIndex = 5;
+            this.linkedLabelLeaderboard.TabStop = true;
+            this.linkedLabelLeaderboard.Text = "Leaderboards";
             // 
             // FormMemoryGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1012, 530);
+            this.Controls.Add(this.linkedLabelLeaderboard);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.Lets_Play);
-            this.Controls.Add(this.LeaderBoard);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.buttonPlay);
             this.Controls.Add(this.Memory_Game_picture);
             this.Controls.Add(this.Player_Name);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.buttonReturnToMenu);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMemoryGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormMemoryGame";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMemoryGame_FormClosed);
+            this.Load += new System.EventHandler(this.FormMemoryGame_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Memory_Game_picture)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,13 +180,13 @@ namespace csharp_vathmologoumeni_3
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonReturnToMenu;
+        private System.Windows.Forms.TextBox textBoxUsername;
         private System.Windows.Forms.Label Player_Name;
         private System.Windows.Forms.PictureBox Memory_Game_picture;
-        private System.Windows.Forms.Label LeaderBoard;
-        private System.Windows.Forms.Button Lets_Play;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.LinkLabel linkedLabelLeaderboard;
     }
 }
--- FormMemoryGame.cs
+++ FormMemoryGame.cs
@@ -12,25 +12,49 @@ namespace csharp_vathmologoumeni_3
 {
     public partial class FormMemoryGame : Form
     {
-        public static int random;
+        bool TransitionExit = false;
+
         public FormMemoryGame()
         {
             InitializeComponent();
         }
+        private void FormMemoryGame_Load(object sender, EventArgs e)
+        {
+            //θέσε το τσεκμποξ να είναι checked και το κουμπί να είναι απενεργοποιημένο (αφού στην αρχή δεν υπάρχει username).
+            checkBox1.Checked  = true;
+            buttonPlay.Enabled = false;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            TransitionExit = false;
             Close();
-        }
-
-        private void Lets_Play_Click(object sender, EventArgs e)
-        {
-            
         }
 
         private void FormMemoryGame_FormClosed(object sender, FormClosedEventArgs e)
         {
-            Application.OpenForms[0].Show();
+            if (!TransitionExit)
+                Application.OpenForms[0].Show();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            numericUpDownMinutes.Enabled = checkBox1.Checked;
+        }
+
+        private void textBoxUsername_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxUsername.TextLength == 0)
+                buttonPlay.Enabled = false;
+            else
+                buttonPlay.Enabled = true;
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            new MemoryGame_GamePlay().Show();
+            TransitionExit = true;
+            Close();
         }
     }
 }
