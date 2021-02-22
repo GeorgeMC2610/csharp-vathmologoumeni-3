f47ee66662c050ecb5d332a29f62aa95bef69cf8
Left123456 <lefterislamprakos36@gmail.com>
2021-02-22 02:31:28 +0200

    Leaderboard form added

---
+++ MemoryGame_Leaderboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_vathmologoumeni_3
+{
+    public partial class MemoryGame_Leaderboard : Form
+    {
+        public MemoryGame_Leaderboard()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReturnToMenu_Click(object sender, EventArgs e)
+        {
+            new FormMemoryGame().Show();
+            
+            Close(); 
+        }
+    }
+}
