959c9533fe804018568dcbb5e9fd08e478c73e20
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-02-22 13:51:41 +0200

    implemented game loading & saving

--- MemoryGameIO.cs
+++ MemoryGameIO.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace csharp_vathmologoumeni_3
 {
@@ -11,10 +12,32 @@ namespace csharp_vathmologoumeni_3
     {
         public static void SaveGame(string name, int pairs, int tries, int timeInitial, int timeRemaining)
         {
-            StreamWriter sw = new StreamWriter("leaderboards.txt", true);
-            string textToWrite = name + "|" + pairs.ToString() + "|" + tries.ToString() + "|" + timeInitial.ToString() + "|" + timeRemaining.ToString();
-            sw.WriteLine(textToWrite);
-            sw.Close();
+            try
+            {
+                StreamWriter sw = new StreamWriter("leaderboards.txt", true, Encoding.UTF8);
+                string textToWrite = name + "|" + pairs.ToString() + "|" + tries.ToString() + "|" + timeInitial.ToString() + "|" + timeRemaining.ToString();
+                sw.WriteLine(textToWrite);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unable to save game.\n\nException Error: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static string LoadStats()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("leaderboards.txt");
+                string Stats = sr.ReadToEnd();
+                return Stats;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to load stats.\n\nException Error: " + e.Message);
+                return "empty";
+            }
         }
     }
 }
